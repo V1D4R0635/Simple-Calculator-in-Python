da56afb835ced9a397d183b791047e92f24845e6
V1D4R0635 <149069239+V1D4R0635@users.noreply.github.com>
2024-02-13 13:16:43 +0800

    Simple Calculator.py

---
+++ Simple Calculator.py
@@ -0,0 +1,12 @@
+x = float(input("First Number: "))
+y = float(input("Second Number: "))
+
+Sum = x + y
+Diff = x - y
+Prod = x * y 
+Quo = x / y
+
+print(f"Sum: {Sum}")
+print(f"Difference: {Diff}")
+print(f"Product: {Prod}")
+print(f"Ouotient: {Quo}")
